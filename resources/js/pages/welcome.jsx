6bafc0bf6fac57739ab7bb7de613807dcd495ff3
forkanimahdi <forkanimahdi@gmail.com>
2025-09-18 12:08:36 +0100

    fix : welcome  page  types

--- resources/js/pages/welcome.jsx
+++ resources/js/pages/welcome.jsx
@@ -1,9 +1,8 @@
 import { dashboard, login, register } from '@/routes';
-import { type SharedData } from '@/types';
 import { Head, Link, usePage } from '@inertiajs/react';
 
 export default function Welcome() {
-    const { auth } = usePage<SharedData>().props;
+    const { auth } = usePage().props;
 
     return (
         <>
