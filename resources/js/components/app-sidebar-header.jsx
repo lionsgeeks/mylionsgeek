5e4b4d18548dd52b5a309a5a741b9af17058bf66
forkanimahdi <forkanimahdi@gmail.com>
2025-09-24 10:03:27 +0100

    change side bar

--- resources/js/components/app-sidebar-header.jsx
+++ resources/js/components/app-sidebar-header.jsx
@@ -20,8 +20,8 @@ export function AppSidebarHeader({ breadcrumbs = [] }) {
     return (
         <header className="flex h-16 shrink-0 items-center justify-between gap-2 border-b border-sidebar-border/50 px-6 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12 md:px-4">
             <div className="flex items-center gap-2">
-                <SidebarTrigger className="-ml-1" />
-                <Breadcrumbs breadcrumbs={breadcrumbs} />
+                {/* <SidebarTrigger className="-ml-1" /> */}
+                {/* <Breadcrumbs breadcrumbs={breadcrumbs} /> */}
             </div>
             <div className="flex w-full items-center justify-between pl-4">
                 <div className="flex flex-col leading-tight">
