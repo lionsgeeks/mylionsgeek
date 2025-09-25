9b3c24158f65ffe6dd3c3b0f55fbbc24ef3d3d30
youssef-labib <youssef.labib2023@gmail.com>
2025-09-25 12:46:15 +0100

    fix: fixed the light mode issue in login & forgot-password

--- resources/js/pages/auth/login.jsx
+++ resources/js/pages/auth/login.jsx
@@ -65,7 +65,7 @@ export default function Login({ status, canResetPassword }) {
 
                             <Button
                                 type="submit"
-                                className="mt-4 w-full bg-[var(--color-alpha)] text-black hover:brightness-95 dark:text-black"
+                                className="mt-4 w-full bg-neutral-900 text-white hover:bg-black dark:bg-[var(--color-alpha)] dark:text-black dark:hover:brightness-95"
                                 tabIndex={4}
                                 disabled={processing}
                                 data-test="login-button"
