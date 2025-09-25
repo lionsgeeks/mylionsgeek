0b3cb45117cd11ec37342db75ff36c9f16b257b8
forkanimahdi <forkanimahdi@gmail.com>
2025-09-25 16:18:28 +0100

    feature : add  full calendar  package with a demo test in  dashboard

--- resources/js/pages/dashboard.jsx
+++ resources/js/pages/dashboard.jsx
@@ -32,14 +32,24 @@ export default function Dashboard() {
                     </div>
                 </div>
                 <div className="relative min-h-[100vh] flex-1 overflow-hidden rounded-xl border border-sidebar-border/70 md:min-h-min dark:border-sidebar-border">
+
                     <FullCalendar
-                        plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
-                        initialView="dayGridMonth"
-                        selectable={true}
-                        editable={true} // allows drag/drop
-                        events={[]} // pass Laravel events
+                        plugins={[timeGridPlugin, interactionPlugin, dayGridPlugin]}
+                        initialView="timeGridWeek"         // shows week view with hours
+                        headerToolbar={{
+                            left: "prev,next today",
+                            center: "title",
+                            right: "dayGridMonth,timeGridWeek,timeGridDay", // buttons like in your screenshot
+                        }}
+                        allDaySlot={true}                  // shows the "all-day" row
+                        slotMinTime="08:00:00"             // earliest hour
+                        slotMaxTime="20:00:00"             // latest hour
+                        selectable={true}                  // allow selecting time slots
+                        editable={true}                    // allow drag/drop
+                        // events={events}                    // your JS array of events
                         eventClick={(info) => alert(`Event: ${info.event.title}`)}
-                        dateClick={(info) => alert(`Clicked date: ${info.dateStr}`)}
+                        dateClick={(info) => console.log(`Clicked date: ${info.dateStr}`)}
+                        height="auto"
                     />
 
                 </div>
