22e4dfeb479b3fdf8e4823051dc462eee6386cb5
forkanimahdi <forkanimahdi@gmail.com>
2025-09-25 10:10:38 +0100

    change app layout

--- resources/js/layouts/app-layout.tsx
+++ resources/js/layouts/app-layout.tsx
@@ -1,16 +1,26 @@
-import AppLayoutTemplate from '@/layouts/app/app-sidebar-layout';
-import { type BreadcrumbItem } from '@/types';
-import { type ReactNode } from 'react';
+import { usePage } from "@inertiajs/react";
+import AppHeaderLayout from "@/layouts/app/app-header-layout";
+import AppSidebarLayout from "@/layouts/app/app-sidebar-layout";
+import { type BreadcrumbItem } from "@/types";
+import { type ReactNode } from "react";
 
 interface AppLayoutProps {
     children: ReactNode;
     breadcrumbs?: BreadcrumbItem[];
 }
 
-export default ({ children, breadcrumbs, ...props }: AppLayoutProps) => (
-    <AppLayoutTemplate breadcrumbs={breadcrumbs} {...props}>
-        <div className='bg-light shadow-lg dark:bg-dark w-[96%] my-6 mx-auto h-full  rounded-lg '>
-            {children}
-        </div>
-    </AppLayoutTemplate>
-);
+export default function AppLayout({ children, breadcrumbs, ...props }: AppLayoutProps) {
+    const { auth } = usePage<{ auth: { user: { role: string } } }>().props;
+
+    const isStudentOrCoworker = ["student", "coworker"].includes(auth?.user?.role);
+
+    const Layout = isStudentOrCoworker ? AppHeaderLayout : AppSidebarLayout;
+
+    return (
+        <Layout breadcrumbs={breadcrumbs} {...props}>
+            <div className="bg-light dark:bg-dark shadow-lg w-[96%] my-6 mx-auto h-full rounded-lg">
+                {children}
+            </div>
+        </Layout>
+    );
+}
