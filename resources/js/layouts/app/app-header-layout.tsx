d11f51d631934264cedcf13ada3f31c2002bfd3d
forkanimahdi <forkanimahdi@gmail.com>
2025-09-25 10:49:56 +0100

    fix : quality test

--- resources/js/layouts/app/app-header-layout.tsx
+++ resources/js/layouts/app/app-header-layout.tsx
@@ -4,7 +4,7 @@ import { AppShell } from '@/components/app-shell';
 import { type BreadcrumbItem } from '@/types';
 import type { PropsWithChildren } from 'react';
 
-export default function AppHeaderLayout({ children, breadcrumbs }: PropsWithChildren<{ breadcrumbs?: BreadcrumbItem[] }>) {
+export default function AppHeaderLayout({ children }: PropsWithChildren<{ breadcrumbs?: BreadcrumbItem[] }>) {
     return (
         <AppShell>
             <AppHeader  />
