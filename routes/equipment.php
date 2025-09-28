3d40929b5a3785020cbbd6d69c3ec4de22ffe627
youssef-labib <youssef.labib2023@gmail.com>
2025-09-28 23:46:54 +0100

    feat(equipements): added the update and delete feature, and the filter

--- routes/equipment.php
+++ routes/equipment.php
@@ -7,6 +7,8 @@ Route::middleware(['auth','verified','role:admin'])->prefix('admin')->group(func
     // Keep route names the same so existing code using route('admin.equipment*') works
     Route::get('/equipements', [EquipmentController::class, 'index'])->name('admin.equipment');
     Route::post('/equipements', [EquipmentController::class, 'store'])->name('admin.equipment.store');
+    Route::put('/equipements/{equipment}', [EquipmentController::class, 'update'])->name('admin.equipment.update');
+    Route::delete('/equipements/{equipment}', [EquipmentController::class, 'destroy'])->name('admin.equipment.destroy');
 });
 
 
