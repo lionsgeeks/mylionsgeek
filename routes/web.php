3b22e74602899bd56ebdec2142a120938297976c
forkanimahdi <forkanimahdi@gmail.com>
2025-09-29 10:09:48 +0100

    remove test route

--- routes/web.php
+++ routes/web.php
@@ -21,41 +21,8 @@ Route::middleware(['auth', 'verified'])->prefix('admin')->group(function () {
 });
 
 
-use Illuminate\Support\Facades\Http;
-
-Route::get('/wakatime-data', function () {
-    // $apiKey = 'waka_d8277e8a-1e05-4178-a9e7-a7feeac8cb36';
-
-    $query = User::whereNotNull('wakatime_api_key');
-
-    $users = $query->get();
 
 
-    $startDate = now()->startOfWeek()->toDateString(); // e.g. 2025-09-21
-    $endDate   = "2025-09-25";
-
-
-    // dd($endDate);
-    foreach ($users as $user) {
-
-        $apiKey = $user->wakatime_api_key;
-        $response = Http::withHeaders([
-            'Authorization' => 'Basic ' . base64_encode($apiKey . ':'),
-        ])->get('https://wakatime.com/api/v1/users/current/summaries', [
-            'start' => $startDate,
-            'end' => $endDate,
-        ]);
-
-        $data = $response->json();
-
-        $cumulative = $data['cumulative_total'];
-
-        dd($data);
-    }
-    // Fetch data from WakaTime API
-
-    // Dump the response
-});
 require __DIR__ . '/settings.php';
 require __DIR__ . '/auth.php';
 require __DIR__ . '/users.php';
