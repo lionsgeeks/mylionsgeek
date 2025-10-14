b38d4ff35f606c9e04bb3bfe878ef3efbad7d783
yahya mws <yahyamoussair05@gmail.com>
2025-10-14 23:06:50 +0100

    feat : make custome mail for all emails we need v0

--- resources/views/emails/CompleteProfile.blade.php
+++ resources/views/emails/CompleteProfile.blade.php
@@ -1,2 +1,12 @@
- <h1>Hello, {{ $user->name }}</h1>
- <p>Thanks for joining our platform!</p>
+@extends('emails.layouts.customMail')
+
+@section('content')
+    <h2>Hello {{ $user->name }}!</h2>
+    <p>Welcome to LionsGeek 🎉</p>
+    {{-- <p>Here is your temporary password: <strong>{{ $password }}</strong></p> --}}
+    <p>Please change it after logging in.</p>
+
+    <div style="text-align: center; margin-top: 20px;">
+        <a href="https://lionsgeek.ma/login" class="btn">Login Now</a>
+    </div>
+@endsection
