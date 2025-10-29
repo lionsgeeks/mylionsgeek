f4a1e6b926df5b31379c3aaaec880ea7b58d51f4
yahya mws <yahyamoussair05@gmail.com>
2025-10-29 17:17:11 +0100

    feat : add show password to login page

--- resources/js/pages/auth/login.jsx
+++ resources/js/pages/auth/login.jsx
@@ -10,13 +10,15 @@ import AuthLayout from '@/layouts/auth-layout';
 // import { register } from '@/routes';
 import { request } from '@/routes/password';
 import { Form, Head } from '@inertiajs/react';
-import { LoaderCircle } from 'lucide-react';
+import { Eye, EyeOff, LoaderCircle } from 'lucide-react';
+import { useState } from 'react';
 
 
 export default function Login({ status, canResetPassword }) {
+    const [showConfirmPassword, setShowConfirmPassword] = useState(false)
     const handleCustomErrors = (responseErrors) => {
         if (responseErrors.email) {
-           alert("h")
+            alert("h")
         }
     };
     return (
@@ -54,15 +56,24 @@ export default function Login({ status, canResetPassword }) {
                                             </TextLink>
                                         )}
                                     </div>
-                                    <Input
-                                        id="password"
-                                        type="password"
-                                        name="password"
-                                        required
-                                        tabIndex={2}
-                                        autoComplete="current-password"
-                                        placeholder="Password"
-                                    />
+                                    <div className='relative'>
+                                        <Input
+                                            id="password"
+                                            type={showConfirmPassword ? "text" : "password"}
+                                            name="password"
+                                            required
+                                            tabIndex={2}
+                                            autoComplete="current-password"
+                                            placeholder="Password"
+                                        />
+                                        <button
+                                            type="button"
+                                            onClick={() => setShowConfirmPassword(!showConfirmPassword)}
+                                            className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-500 hover:text-gray-700"
+                                        >
+                                            {showConfirmPassword ? <EyeOff size={20} /> : <Eye size={20} />}
+                                        </button>
+                                    </div>
                                     <InputError message={errors.password} />
                                 </div>
 
