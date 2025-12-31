00e2ca2bcd25b87f5180e9414817d7bb6d5cb83e
yahya-moussair <yahyamoussair05@gmail.com>
2025-12-31 15:08:09 +0100

    fix : remove message from the full user profile

--- resources/js/components/EXP'S/courses_modal.jsx
+++ resources/js/components/EXP'S/courses_modal.jsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
 import { useForm, router } from '@inertiajs/react';
-import { Plus } from "lucide-react";
+import { Plus, X } from "lucide-react";
 
 export default function CoursesModal() {
   const [open, setOpen] = useState(false);
