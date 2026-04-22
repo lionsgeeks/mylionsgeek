eb60c4e777fb59a1db5eaf54dc1aadd76b342790
yahya-moussair <yahyamoussair05@gmail.com>
2026-04-22 16:19:16 +0100

    feat: add onEditedFileInputChange prop to PostCommentItem component

--- resources/js/components/post/comments/PostCommentItem.jsx
+++ resources/js/components/post/comments/PostCommentItem.jsx
@@ -17,6 +17,7 @@ export default function PostCommentItem({
     editedCommentImagePreview,
     editedRemoveImage,
     compressingEditedImage,
+    onEditedFileInputChange,
     onRemoveEditedPreview,
     onMarkRemoveExistingImage,
     expandedCommentIds,
