dd0a6a4e5b600fd1ecbac37ed5ebd6f31491ee58
yahya-moussair <yahyamoussair05@gmail.com>
2026-06-26 18:13:29 +0100

    feat: refactor announcement controller and update route references

--- routes/admin/announcements.php
+++ routes/admin/announcements.php
@@ -1,15 +1,15 @@
 <?php
 
-use App\Http\Controllers\AnnouncementController;
+use App\Http\Controllers\Admin\AnnouncementController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth:sanctum', 'role:admin'])->prefix('/admin')->group(function () {
-    // get send notification for web
+    //     // get send notification for web
     Route::get('/announcements', [AnnouncementController::class, 'index']);
     // post send notification for 
     Route::post('/announcements/store', [AnnouncementController::class, 'store']);
-    // update send notification for 
+    // // update send notification for 
     Route::put('/announcements/update/{id}', [AnnouncementController::class, 'update']);
-    // delete send notification for 
+    // // delete send notification for 
     Route::delete('/announcements/destroy/{id}', [AnnouncementController::class, 'destroy']);
 });
