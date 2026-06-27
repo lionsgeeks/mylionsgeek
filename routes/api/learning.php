682a7e31276aaffd0a73993999a6fd07c0fdda3e
Grandgg <ab.naciri.official@gmail.com>
2026-06-27 01:44:00 +0100

    feat: added socials and WakatimeKeys Routes

--- routes/api/learning.php
+++ routes/api/learning.php
@@ -2,10 +2,14 @@
 
 use App\Http\Controllers\API\LearningController;
 use App\Http\Controllers\SendClassController;
+use App\Http\Controllers\SendSocialsController;
+use App\Http\Controllers\SendWakaTimeController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware("learning")->group(function()
 {
     Route::get("/academy/token", [LearningController::class, "handleToken"]);
     Route::get("/academy/classes",[SendClassController::class, "GetClassesData"]);
+    Route::get("/academy/socials",[SendSocialsController::class, "send"]);
+    Route::get("/academy/wakatime",[SendWakaTimeController::class, "send"]);
     });
